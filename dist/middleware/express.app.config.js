7eb40d6493e6f85cf4eb30b2cc1d47e644fb3ece
Shivam Parashar <shivamparashar33@gmail.com>
2023-08-30 12:18:34 +0530

    Package JSON updated to fix the audit issues (#1)
    * Package file update
    
    * Package main changed
    
    * Package main changed
    
    * Dist added for test
    
    ---------
    
    Co-authored-by: Shivam Parashar <shivam.parashar@monotype.com>

--- dist/middleware/express.app.config.js
+++ dist/middleware/express.app.config.js
@@ -1,101 +1,79 @@
 'use strict';
-
-import * as express from 'express';
-import cookieParser = require('cookie-parser');
-import bodyParser = require('body-parser');
-import cors = require('cors');
-import { SwaggerUI } from './swagger.ui';
-import { SwaggerRouter } from './swagger.router';
-import { SwaggerParameters } from './swagger.parameters';
-import * as logger from 'morgan';
-import * as fs from 'fs';
-import * as jsyaml from 'js-yaml';
-import * as OpenApiValidator from 'express-openapi-validator';
-import { Oas3AppOptions } from './oas3.options';
-import { OpenApiRequestHandler } from 'express-openapi-validator/dist/framework/types'
-
-export class ExpressAppConfig {
-    private app: express.Application;
-    private routingOptions;
-    private definitionPath;
-    private openApiValidatorOptions;
-
-    constructor(definitionPath: string, appOptions: Oas3AppOptions, customMiddlewares?: OpenApiRequestHandler[]) {
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.ExpressAppConfig = void 0;
+const express = require("express");
+const cookieParser = require("cookie-parser");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const swagger_ui_1 = require("./swagger.ui");
+const swagger_router_1 = require("./swagger.router");
+const swagger_parameters_1 = require("./swagger.parameters");
+const logger = require("morgan");
+const fs = require("fs");
+const jsyaml = require("js-yaml");
+const OpenApiValidator = require("express-openapi-validator");
+class ExpressAppConfig {
+    constructor(definitionPath, appOptions, customMiddlewares) {
         this.definitionPath = definitionPath;
         this.routingOptions = appOptions.routing;
         this.setOpenApiValidatorOptions(definitionPath, appOptions);
-
         // Create new express app only if not passed by options
         this.app = appOptions.app || express();
-
         this.app.use(cors(appOptions.cors));
-
         const spec = fs.readFileSync(definitionPath, 'utf8');
         const swaggerDoc = jsyaml.load(spec);
-
         this.app.use(bodyParser.urlencoded());
         this.app.use(bodyParser.text());
         this.app.use(bodyParser.json());
         this.app.use(bodyParser.raw({ type: 'application/pdf' }));
-
         this.app.use(this.configureLogger(appOptions.logging));
         this.app.use(express.json());
         this.app.use(express.urlencoded({ extended: false }));
         this.app.use(cookieParser());
-
-        const swaggerUi = new SwaggerUI(swaggerDoc, appOptions.swaggerUI);
+        const swaggerUi = new swagger_ui_1.SwaggerUI(swaggerDoc, appOptions.swaggerUI);
         this.app.use(swaggerUi.serveStaticContent());
-
         this.app.use(OpenApiValidator.middleware(this.openApiValidatorOptions));
-        this.app.use(new SwaggerParameters().checkParameters());
+        this.app.use(new swagger_parameters_1.SwaggerParameters().checkParameters());
         // Bind custom middlewares which need access to the OpenApiRequest context before controllers initialization
-        (customMiddlewares || []).forEach(middleware => this.app.use(middleware))
-        this.app.use(new SwaggerRouter().initialize(this.routingOptions));
-
+        (customMiddlewares || []).forEach(middleware => this.app.use(middleware));
+        this.app.use(new swagger_router_1.SwaggerRouter().initialize(this.routingOptions));
         this.app.use(this.errorHandler);
     }
-
-    private setOpenApiValidatorOptions(definitionPath: string, appOptions: Oas3AppOptions) {
+    setOpenApiValidatorOptions(definitionPath, appOptions) {
         //If no options or no openApiValidator Options given, create empty options with api definition path
         if (!appOptions || !appOptions.openApiValidator) {
             this.openApiValidatorOptions = { apiSpec: definitionPath };
             return;
         }
-
         // use the given options
         this.openApiValidatorOptions = appOptions.openApiValidator;
-
         // Override apiSpec with definition Path to keep the prior behavior
         this.openApiValidatorOptions.apiSpec = definitionPath;
     }
-
-    public configureLogger(loggerOptions) {
+    configureLogger(loggerOptions) {
         let format = 'dev';
-        let options: {} = {};
-
+        let options = {};
         if (loggerOptions != undefined) {
             if (loggerOptions.format != undefined
                 && typeof loggerOptions.format === 'string') {
                 format = loggerOptions.format;
             }
-
             if (loggerOptions.errorLimit != undefined
                 && (typeof loggerOptions.errorLimit === 'string' || typeof loggerOptions.errorLimit === 'number')) {
                 options['skip'] = function (req, res) { return res.statusCode < parseInt(loggerOptions.errorLimit); };
             }
         }
-
         return logger(format, options);
     }
-
-    private errorHandler(error, request, response, next) {
+    errorHandler(error, request, response, next) {
         response.status(error.status || 500).json({
             message: error.message,
             errors: error.errors,
         });
     }
-
-    public getApp(): express.Application {
+    getApp() {
         return this.app;
     }
 }
+exports.ExpressAppConfig = ExpressAppConfig;
+//# sourceMappingURL=express.app.config.js.map
